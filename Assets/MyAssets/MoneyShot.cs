a544ec12dea89620fd59941c25fa169de1b2c3ac
LawrenceTurnerW <watawata.takatan@gmail.com>
2024-03-18 01:35:11 +0900

    再発射可能時間を設定できるように

--- Assets/MyAssets/MoneyShot.cs
+++ Assets/MyAssets/MoneyShot.cs
@@ -13,32 +13,44 @@ public class NewBehaviourScript : MonoBehaviour
     // 発射するプレハブ
     [SerializeField] private GameObject m_moneyPrefab = null;
 
-    void Start()
-    {
+    private bool canShot = true;
 
-    }
+    void Start() { }
 
     void Update()
     {
-        // 右手のポジションを取得
-        Vector3 lTargetPos = lTarget.transform.position;
-
-        // 左手ポジションを取得
-        Vector3 rTargetPos = rTarget.transform.position;
-
-        // 距離を取得
-        float dis = Vector3.Distance(lTargetPos, rTargetPos);
-
-        // もしAとBが2cm以下にあれば発射判定
-        if (dis <= 0.05)
+        // 発射可能なときのみポジションを取得する
+        if (canShot)
         {
-            // 左手の中指の正の方向に対して発射する
-            GameObject money = Instantiate(m_moneyPrefab);
+            // 右手のポジションを取得
+            Vector3 lTargetPos = lTarget.transform.position;
+            // 左手ポジションを取得
+            Vector3 rTargetPos = rTarget.transform.position;
+            // 距離を取得
+            float dis = Vector3.Distance(lTargetPos, rTargetPos);
 
-            // 位置と発射方向を発射点に合わせる
-            money.transform.position = m_lunchPos.position;
-            money.transform.rotation = m_lunchPos.rotation;
+            // もしAとBが5cm以下にあれば発射判定
+            if (dis <= 0.05)
+            {
+                // 左手の中指の正の方向に対して発射する
+                GameObject money = Instantiate(m_moneyPrefab);
+
+                // 位置と発射方向を発射点に合わせる
+                money.transform.position = m_lunchPos.position;
+                money.transform.rotation = m_lunchPos.rotation;
+
+                //再発射不可能にして、0.5秒後に再発射可能にする
+                canShot = false;
+                StartCoroutine(CanBeShot());
+
+            }
         }
+    }
 
+    // コルーチン
+    private IEnumerator CanBeShot()
+    {
+        yield return new WaitForSeconds(0.3f);
+        canShot = true;
     }
 }
